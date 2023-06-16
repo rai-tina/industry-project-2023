6b6f06d1b4bd576275a8248fe41ad5597eb9a278
gajeev <gajeev@gmail.com>
2023-06-16 06:33:22 -0400

    setup top level pages/components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+// import Inventory from "./pages/Inventory/Inventory";
+// import Warehouse from "./pages/Warehouse/Warehouse";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+import MainPage from "./pages/MainPage/MainPage";
+import LeaderboardPage from "./pages/LeaderboardPage/LeaderboardPage";
 import "./App.scss";
 
-function App() {
-  return <div className="App"></div>;
-}
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/dashboard" element={<MainPage />}></Route>
+        <Route path="/leaderboard" element={<LeaderboardPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
