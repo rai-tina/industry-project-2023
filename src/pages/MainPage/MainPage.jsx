807f37446a6819a94d51b2c3a47b82dd0cb75d70
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-16 10:51:06 -0400

    Complete savings section of main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,14 @@
 import "./MainPage.scss";
 
 // Import Images
-import userMohan from "../../assets/images/Mohan-muruge.jpg";
 
 // Import Icons
 import cloud from "../../assets/icons/cloud-svg.svg";
+import emily from "../../assets/images/emily-greenway.svg";
+import logo from "../../assets/icons/logo.svg";
+
+// Import components
+import Footer from "../../components/Footer/Footer";
 
 export default function MainPage() {
   return (
@@ -17,8 +21,8 @@ export default function MainPage() {
             <hr />
             <hr />
           </div>
-          <img src={userMohan} alt="mohan" className="main__user" />
-          <img src={userMohan} alt="logo" className="main__logo" />
+          <img src={emily} alt="mohan" className="main__user" />
+          <img src={logo} alt="logo" className="main__logo" />
         </div>
 
         <h1 className="main__welcome-text">Welcome back Emily!</h1>
@@ -26,21 +30,41 @@ export default function MainPage() {
         <div className="main__savings">
           <h2 className="main__savings-title">C02 Saved</h2>
           <div className="main__savings-stats">
-            <div className="main__savings-pie">84% Compared to last month</div>
-            <div className="main__savings-emission">
-              <img src={cloud} alt="cloud" className="main__savings-icon" />
-              Current emission{" "}
-              <span className="main__savings-amount">24kg</span>
+            <div className="main__savings-pie">
+              <span className="main__savings-percent">84%</span>
+
+              <span className="main__savings-compared">
+                Compared to last month
+              </span>
             </div>
 
-            <div className="main__savings-emission">
-              <img src={cloud} alt="cloud" className="main__savings-icon" />
-              Last months emission{" "}
-              <span className="main__savings-amount">24kg</span>
+            <div className="main__savings-emissions">
+              <div className="main__savings-emission">
+                <img src={cloud} alt="cloud" className="main__savings-icon" />
+
+                <div className="main__savings-message">
+                  <span className="main__savings-text">Current emission</span>
+                  <span className="main__savings-amount">24kg</span>
+                </div>
+              </div>
+
+              <div className="main__savings-emission">
+                <img src={cloud} alt="cloud" className="main__savings-icon" />
+
+                <div className="main__savings-message">
+                  <span className="main__savings-text">
+                    Last month's emission
+                  </span>
+                  <span className="main__savings-amount">24kg</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
+        
       </main>
+      <Footer />
     </>
   );
 }
