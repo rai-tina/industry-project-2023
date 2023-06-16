9a7e48ecb7d230ca8df73a870c2c4fde972616d2
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-16 09:32:23 -0400

    Install react-router-dom, export HomePage, setup MainPage boilerplate

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,28 @@
 export default function MainPage() {
-  return <></>;
+  return (
+    <>
+      <main className="main">
+        <div className="main__welcome-content">
+          <div className="main__menu">
+            <hr />
+            <hr />
+            <hr />
+          </div>
+          <img src="" alt="" className="main__user" />
+          <img src="" alt="" className="main__logo" />
+        </div>
+
+        <h1 className="main__welcome-text">Welcome back Emily!</h1>
+
+        <div className="main__savings">
+          <h2 className="main__savings-title">C02 Saved</h2>
+          <div className="main__savings-stats">
+            <div className="main__savings-pie">84% Compared to last month</div>
+            <div className="main__savings-emission"></div>
+            <div className="main__savings-emission"></div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
