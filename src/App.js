de403077f5f49839842cc48f0aaa9272f2837d9b
gajeev <gajeev@gmail.com>
2023-06-16 06:46:33 -0400

    fix app.js bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-// import Inventory from "./pages/Inventory/Inventory";
-// import Warehouse from "./pages/Warehouse/Warehouse";
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
 import MainPage from "./pages/MainPage/MainPage";
 import LeaderboardPage from "./pages/LeaderboardPage/LeaderboardPage";
 import "./App.scss";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/dashboard" element={<MainPage />}></Route>
