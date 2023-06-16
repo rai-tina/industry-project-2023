34abc4734ccc75a2feb21df94f90782231abb4d5
Tina Thakur <tina.thakur.acc@gmail.com>
2023-06-16 11:32:22 -0400

    finish styling homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,11 +10,13 @@ export default function HomePage() {
         src={iphoneBackground}
         alt="image of the background of an iphone"
       ></img>
-      <img
-        className="home-page__notification"
-        src={notification}
-        alt="image of notification message"
-      ></img>
+      <a className="home-page__link" href="">
+        <img
+          className="home-page__link-notification"
+          src={notification}
+          alt="image of notification message"
+        ></img>
+      </a>
     </div>
   );
 }
