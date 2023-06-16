f3430bcc490389884494ccc6f247640283ed3584
Jonathan Mak <j.m4k119@gmail.com>
2023-06-16 12:03:26 -0400

    style

--- src/pages/LeaderboardPage/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -18,64 +18,74 @@ import Footer from "../../components/Footer/Footer";
 const LeaderboardPage = () => {
     return (
         <>
-        <section className="leaderboard">
+            <section className="leaderboard">
 
-            <h1 className="leaderboard__title">Community</h1>
+                <h1 className="leaderboard__title">Community</h1>
 
-            <div className="leaderboard__heading-container">
-                <img className="leaderboard__logo" src={logo} />
-                <p className="leaderboard__month">June</p>
-                <p className="leaderboard__carbon-emmission">CO2e(Kg)</p>
-            </div>
-
-            <p className="leaderboard__caption">Have some fun competing with your friends!</p>
-
-            <div className="leaderboard__user-container">
-                <img className="leaderboard__avatar" src= {emily} />
-                <div className="leaderboard__details-container">
-                    <h2 className="leaderboard__name">Emily Greenway</h2>
-                    <p className="leaderboard__user-emmission">24kg</p>
-                    
+                <div className="leaderboard__heading-container">
+                    <img className="leaderboard__logo" src={logo} />
+                    <p className="leaderboard__month">June</p>
+                    <p className="leaderboard__carbon-emmission">CO2e(Kg)</p>
                 </div>
-            </div>
 
-            <div className="leaderboard__user-container">
-                <img className="leaderboard__avatar" src= {jen} />
-                <div className="leaderboard__details-container">
-                    <h2 className="leaderboard__name">Jen Rosenbaum</h2>
-                    <p className="leaderboard__user-emmission">65kg</p>
-                    <img className="leaderboard__progress-bar" src= {jenBar} />
-                </div>
-            </div>
+                <p className="leaderboard__caption">Have some fun competing with your friends!</p>
 
-            <div className="leaderboard__user-container">
-                <img className="leaderboard__avatar" src= {jon} />
-                <div className="leaderboard__details-container">
-                    <h2 className="leaderboard__name">Jon Marshall</h2>
-                    <p className="leaderboard__user-emmission">80kg</p>
-                    <img className="leaderboard__progress-bar" src= {jonBar} />
-                </div>
-            </div>
+                <div className="leaderboard__user-container">
+                    <img className="leaderboard__avatar" src={emily} />
+                    <div className="leaderboard__details-container">
+                        <div className="leaderboard__user-details">
+                            <h2 className="leaderboard__name">Emily Greenway</h2>
+                            <p className="leaderboard__user-emmission">24kg</p>
+                        </div>
 
-            <div className="leaderboard__user-container">
-                <img className="leaderboard__avatar" src= {joan} />
-                <div className="leaderboard__details-container">
-                    <h2 className="leaderboard__name">Joan Lanster</h2>
-                    <p className="leaderboard__user-emmission">85kg</p>
-                    <img className="leaderboard__progress-bar" src= {joanBar} />
+                    </div>
                 </div>
-            </div>
 
-            <div className="leaderboard__user-container">
-                <img className="leaderboard__avatar" src= {jessie} />
-                <div className="leaderboard__details-container">
-                    <h2 className="leaderboard__name">Jessie Framer</h2>
-                    <p className="leaderboard__user-emmission">90kg</p>
-                    <img className="leaderboard__progress-bar" src= {jessieBar} />
+                <div className="leaderboard__user-container">
+                    <img className="leaderboard__avatar" src={jen} />
+                    <div className="leaderboard__details-container">
+                        <div className="leaderboard__user-details">
+                            <h2 className="leaderboard__name">Jen Rosenbaum</h2>
+                            <p className="leaderboard__user-emmission">65kg</p>
+                        </div>
+                        <img className="leaderboard__progress-bar" src={jenBar} />
+                    </div>
                 </div>
-            </div>
-        </section>
-        <Footer/>
+
+                <div className="leaderboard__user-container">
+                    <img className="leaderboard__avatar" src={jon} />
+                    <div className="leaderboard__details-container">
+                        <div className="leaderboard__user-details">
+                            <h2 className="leaderboard__name">Jon Marshall</h2>
+                            <p className="leaderboard__user-emmission">80kg</p>
+                        </div>
+                        <img className="leaderboard__progress-bar" src={jonBar} />
+                    </div>
+                </div>
+
+                <div className="leaderboard__user-container">
+                    <img className="leaderboard__avatar" src={joan} />
+                    <div className="leaderboard__details-container">
+                        <div className="leaderboard__user-details">
+                            <h2 className="leaderboard__name">Joan Lanster</h2>
+                            <p className="leaderboard__user-emmission">85kg</p>
+                        </div>
+                        <img className="leaderboard__progress-bar" src={joanBar} />
+                    </div>
+                </div>
+
+                <div className="leaderboard__user-container">
+                    <img className="leaderboard__avatar" src={jessie} />
+                    <div className="leaderboard__details-container">
+                        <div className="leaderboard__user-details">
+                            <h2 className="leaderboard__name">Jessie Framer</h2>
+                            <p className="leaderboard__user-emmission">90kg</p>
+                        </div>
+                        <img className="leaderboard__progress-bar" src={jessieBar} />
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
     )
 }
