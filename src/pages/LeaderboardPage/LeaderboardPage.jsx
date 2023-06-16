48e347a3107f34da7e2c78bd3b0fbb13c986f414
Jonathan Mak <j.m4k119@gmail.com>
2023-06-16 09:54:34 -0400

    initial commit structure

---
+++ src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -0,0 +1,19 @@
+import "./LeaderboardPage.scss"
+
+const LeaderboardPage = () => {
+    return (
+        <section className="leaderboard">
+            <h1 className="leaderboard__title">Community</h1>
+            <div className="leaderboard__heading-container">
+                <img className="leaderboard__logo">
+
+                </img>
+                <p className="leaderboard__month">June</p>
+                <p className="leaderboard__carbon-emmission">CO2e (Kg)</p>
+            </div>
+        </section>
+
+    )
+}
+
+export default LeaderboardPage
