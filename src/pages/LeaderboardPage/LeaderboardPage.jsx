29e1cbb32bb2f9894b92a92b4204aee14e5a7b27
Jonathan Mak <j.m4k119@gmail.com>
2023-06-16 10:15:02 -0400

    add pictures to image file and icon

--- src/pages/LeaderboardPage/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -1,11 +1,12 @@
 import "./LeaderboardPage.scss"
+import logo from "../../assets/icons/logo.svg"
 
 const LeaderboardPage = () => {
     return (
         <section className="leaderboard">
             <h1 className="leaderboard__title">Community</h1>
             <div className="leaderboard__heading-container">
-                <img className="leaderboard__logo">
+                <img className="leaderboard__logo" src={logo}>
 
                 </img>
                 <p className="leaderboard__month">June</p>
