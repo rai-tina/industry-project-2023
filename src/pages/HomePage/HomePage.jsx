698178cfa8fdd5a62e468027a75a351a2e79e00a
Tina Thakur <tina.thakur.acc@gmail.com>
2023-06-16 11:15:12 -0400

    style the HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
+import "./HomePage.scss";
+import iphoneBackground from "../../assets/images/iphoneBackground.svg";
+import notification from "../../assets/images/notification.svg";
+
 export default function HomePage() {
   return (
-    <>
-      <img className="home-page__background"></img>
-    </>
+    <div className="home-page">
+      <img
+        className="home-page__background"
+        src={iphoneBackground}
+        alt="image of the background of an iphone"
+      ></img>
+      <img
+        className="home-page__notification"
+        src={notification}
+        alt="image of notification message"
+      ></img>
+    </div>
   );
 }
